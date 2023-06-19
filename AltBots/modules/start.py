76054bac25e49b1405684ac2e6254bce6722b337
adarshUC <adarshuc595@gmail.com>
2023-06-19 16:14:43 +0530

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -1,18 +1,15 @@
-from telethon import __version__, events, Button
+from telethon import version, events, Button
 
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 
 START_BUTTON = [
     [
-        Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
+        Button.inline("️️⚝ нєℓρ ⚝", data="help_back")
     ],
     [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/TheAltron"),
-        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/AltronChats")
-    ],
-    [
-        Button.url("• ʀᴇᴘᴏ •", "https://github.com/ItZxSTaR/XBOTS")
+        Button.url("️️⚝ σωηєя ️️⚝", "https://t.me/aadarsh_legend"),
+        Button.url("️️⚝ ¢нαт ️️️️⚝", "https://t.me/t34m_xd_chat")
     ]
 ]
 
@@ -33,14 +30,14 @@ async def start(event):
         AltBot = await event.client.get_me()
         bot_name = AltBot.first_name
         bot_id = AltBot.id
-        TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝐀xᴇɴ](https://t.me/PyXen)**\n\n"
-        TEXT += f"» **xʙᴏᴛꜱ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
-        TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
-        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
+        TEXT = f"ʜᴇʏ​ {event.sender.first_name},\n\nɪ ᴀᴍ {bot_name}​\n━━━━━━━━━━━━━━━━━━━\n\n"
+        TEXT += f"» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ : 𝝙𝗗𝗔𝗥𝗦𝗛\n\n"
+        TEXT += f"» xᴅʙᴏᴛs ᴠᴇʀsɪᴏɴ : M3.3\n"
+        TEXT += f"» ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : 3.11.3\n"
+        TEXT += f"» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : {version}\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                     event.chat_id,
-                    "https://te.legra.ph/file/07d39b85c6cea32f15259.jpg",
+                    "https://telegra.ph/file/3fbb4043cfc0d3d90e888.jpg",
                     caption=TEXT, 
                     buttons=START_BUTTON
-                )
+        )
