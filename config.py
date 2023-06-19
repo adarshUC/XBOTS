bcc488a08fba53cee4e746a07a84fd012fec3eb1
adarshUC <adarshuc595@gmail.com>
2023-06-19 16:19:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ BOT_TOKEN8 = getenv("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="1153171536").split()))
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="6170423996").split()))
 for x in ALTRON:
     SUDO_USERS.append(x)
 OWNER_ID = int(getenv("OWNER_ID", default="1153171536"))
